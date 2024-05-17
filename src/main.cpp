636ddd9d812557af768a118fc8644aa06252383f
ItsFreSsh <lukes.fr@seznam.cz>
2024-05-17 23:58:27 +0200

    Basic main setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,34 @@
-int main(int argc, char** argv) {
+#include "imageProcessor.h"
+#include "colorSelector.h"
+#include <iomanip>
+#include <cmath>
+#include <Magick++.h>
+#include "../includes/cxxopts/cxxopts.hpp"
+#include "optionsHandler.h"
+
+int main(int argc, char **argv)
+{
+
+    // Initialize option handler
+    OptionsHandler optionsHandler(argc, argv);
+
+    if (optionsHandler.isHelp())
+    {
+        optionsHandler.printHelp();
+        return 0;
+    }
+
+    // Initialize ImageMagick
+    Magick::InitializeMagick(nullptr);
+
+    std::string inputFile = optionsHandler.getInputFile();
+    std::string outputFile = optionsHandler.getOutputFile();
+    int numColors = optionsHandler.getNumColors();
+
+    // Read the input image
+    ImageProcessor imageProcessor(inputFile);
+
+    // Process image..
 
     return 0;
 }
